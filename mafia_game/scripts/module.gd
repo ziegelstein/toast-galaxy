34dfb6c07c5900328a8ce09fd994fe4ab96c9c33
Buli <julius2207@hotmail.de>
2017-05-20 19:17:48 +0200

    minor changes

--- mafia_game/scripts/module.gd
+++ mafia_game/scripts/module.gd
@@ -150,7 +150,7 @@ func get_build_time():
 
 ### Cycle Functions ###
 
-func register_modul():
+func register_module():
 	return global.add_module(self)
 
 func on_cycle_change(modifications, cycle):
