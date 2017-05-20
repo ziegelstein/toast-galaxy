7ac49ce51fcbfafe170ad0f51a02a0a5b81f82f4
Buli <julius2207@hotmail.de>
2017-05-20 17:14:20 +0200

    minor fixes

--- mafia_game/event/event.gd
+++ mafia_game/event/event.gd
@@ -20,7 +20,6 @@ func _init(name, desc, message, weight, requierements, outcomes, eventOptions):
 	self.message = message
 	self.weight = weight
 	self.eventOptions = eventOptions
-	self.requierements = requirements
 	self.requierements = requierements
 	#self.weightmodifiers = weightmodifiers
 	self.outcomes = outcomes
