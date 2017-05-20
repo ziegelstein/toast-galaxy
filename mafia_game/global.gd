735f956a1295cf4fa3782e761b88e57b5e3492bc
Ziegelstein <felix.frings@yahoo.de>
2017-05-20 04:44:11 +0200

    I can't sleep ...

--- mafia_game/global.gd
+++ mafia_game/global.gd
@@ -5,6 +5,7 @@ extends Node
 # var b = "textvar"
 var current_scene = null
 
+var messeges = []
 var resources = {} setget , get_resources
 var station_stats = {"Angriff":0, "Verteidigung":0, "Popularitaet":0, "Verdaechtigkeit":0} setget , get_station_stats
 
@@ -19,3 +20,18 @@ func get_resources():
 
 func get_station_stats():
 	return station_stats
+	
+func get_messeges():
+	return messeges
+
+func get_last_messege():
+	return messeges[messeges.size()-1]
+	
+func add_messege(messege):
+	messeges.append(messege)
+	pass
+
+func add_messeges(messegearray):
+	for messege in messegearray:
+		messeges.append(messege)
+	pass
--- mafia_game/scripts/module.gd
+++ mafia_game/scripts/module.gd
@@ -1,17 +1,17 @@
 extends Node
 
-var name = "generic module"
-var desc = "generic description"
+
+
 var id = 0
-var moduleid = 0 #
+var name = "module" + str(id)
+var modname = "generic module"
+var desc = "generic description"
 var submodules = []
 var buildprice = 0
 var buildmaterials = { none = "" }
 
 
-func _init(name, desc, id):
-	self.name = name
-	self.desc = desc
+func _init(name, id):
 	self.id = id
 	pass
 
@@ -19,3 +19,6 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+	
+func on_cycle_change(modifications, cycle):
+	return true
