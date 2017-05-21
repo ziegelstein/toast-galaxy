41c0631d0756be413e11d5bdd8394999d2dc13b8
Buli <julius2207@hotmail.de>
2017-05-21 11:54:46 +0200

    display updating implemented

--- mafia_game/gui/settings_entry.gd
+++ mafia_game/gui/settings_entry.gd
@@ -18,3 +18,9 @@ func update():
 	get_node("spinbox").set_max(maximum)
 	
 	get_node("spinbox").set_value(module.get_module_settings[resource_name])
+
+func _on_spinbox_value_changed( value ):
+	var settings = module.get_resource_settings()
+	settings[resource_name] = value
+	module.set_resource_settings(settings)
+	global.update_resource_display()
