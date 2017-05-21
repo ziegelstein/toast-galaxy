05c62683cbe46119aba14799b92a022d0f9d3ed6
Buli <julius2207@hotmail.de>
2017-05-21 13:44:33 +0200

    minor changes

--- mafia_game/gui/blueprint_selection.gd
+++ mafia_game/gui/blueprint_selection.gd
@@ -8,3 +8,7 @@ func _ready():
 	var blueprints = global.global_blueprint_reader.get_blueprints()
 	for key in blueprints:
 		add_item(blueprints[key].modname)
+
+func _on_blueprint_selection_item_pressed( ID ):
+	global.add_message(get_item_text(ID))
+	pass # replace with function body
