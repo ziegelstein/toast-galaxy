da52ccf93f115b9baa17df15095642f09b4e51c4
Buli <julius2207@hotmail.de>
2017-05-21 15:11:31 +0200

    minor changes

--- mafia_game/scenes/main_menu_buttons.gd
+++ mafia_game/scenes/main_menu_buttons.gd
@@ -8,3 +8,15 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+
+func _on_start_game_pressed():
+	pass # replace with function body
+
+
+func _on_options_pressed():
+	pass # replace with function body
+
+
+func _on_quit_pressed():
+	pass # replace with function body
